addcb86a8cb04499a5041ba8729683e828d622be
k <mol.tired@gmail.com>
2020-11-08 23:10:59 -0500

    app: add sample by our teacher but performing the builder pattern
    Co-authored-by: JoseHenao1 <jose.henao1@udea.edu.co>
    Co-authored-by: M-Kleber <marfa.kleber@udea.edu.co>

--- src/main/java/co/edu/udea/json/App.java
+++ src/main/java/co/edu/udea/json/App.java
@@ -1,42 +1,55 @@
 package co.edu.udea.json;
 
-import org.json.JSONException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.XML;
 
 public class App 
 {
     public static int IDENT_FACTOR = 4;
-    public static String XML_STRING =
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            " <flights>\n"                             +
-            " <flight>\n"                              +
-            " <from>Barranquilla</from>\n"             +
-            " <to>Medellin</to>\n"                     +
-            " <fare>$175.000j</fare>\n"                +
-            " <aircraft>Boeing 777</aircraft>\n"       +
-            " </flight>\n"                             +
-            " <flight>\n"                              +
-            " <from>Bogota</from>\n"                   +
-            " <to>Cartagena</to>\n"                    +
-            " <fare>$198.000j</fare>\n"                +
-            " <aircraft>Boeing 747</aircraft>\n"       +
-            " </flight>\n"                             +
-            " <flight>\n"                              +
-            " <from>Medellin</from>\n"                 +
-            " <to>Bogota</to>\n"                       +
-            " <fare>$150.000j</fare>\n"                +
-            " <aircraft>Airbus</aircraft>\n"           +
-            " </flight>\n"                             +
-            " </flights>";
 
     public static void main(String[] args) {
+        Vendedor v1 = Vendedor.builder()
+            .nombre("Juan")
+            .apellido("Perez")
+            .edad(10)
+            .build();
+
+        Cliente c1 = Cliente.builder()
+            .nombre("HIVYMAR")
+            .direccion("Victor Emilio Estrada 204")
+            .telefono("5020800")
+            .build();
+
+        Cliente c2 = Cliente.builder()
+            .nombre("PROMESA")
+            .direccion("Via. Daule KM 5.5")
+            .telefono("5013604")
+            .build();
+
+        List<Cliente> clientes = new ArrayList<Cliente>();
+        clientes.add(c1);
+        clientes.add(c2);
+
+        JSONObject objVendedor = new JSONObject(v1);
+        JSONArray objClientes = new JSONArray();
         try {
-            JSONObject jsonFromXML = XML.toJSONObject(XML_STRING);
-            String jsonOut = jsonFromXML.toString(IDENT_FACTOR);
-            System.out.print(jsonOut);
-        } catch (JSONException e) {
-            System.out.print(e.toString());
+            for (Cliente cliente : clientes) {
+                JSONObject objCliente = new JSONObject();
+                objCliente.put("nombre", cliente.getNombre());
+                objCliente.put("direccion", cliente.getDireccion());
+                objCliente.put("telefono", cliente.getTelefono());
+                objClientes.put(objCliente);
+            }
+            JSONObject objVendedorClientes = new JSONObject();
+            objVendedorClientes.put("vendedor", objVendedor);
+            objVendedorClientes.put("clientes", objClientes);
+            System.out.println(objVendedorClientes.toString(IDENT_FACTOR));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
